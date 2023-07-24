22071162a36a64f1768748ced12760d6a33ec1d9
markdave123-py <nwaekwudavid@gmail.com>
2023-07-24 14:53:13 +0100

    todays problem

--- longestSequence.py
+++ longestSequence.py
@@ -7,3 +7,21 @@ class Solution:
             mp[i]=mp[i-difference]+1
             mx=max(mp[i],mx)
         return mx
+
+
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+
+            charSet.add(s[r])
+
+            res = max(res, r - l + 1)
+
+        return lengthOfLon
